f7b8ba4bb5538a35f0ac7b088d79054fd3ade255
unknow 9090 <nsnshanmukh52@gmail.com>
2023-04-30 12:07:41 +0530

    some value

---
+++ src/headerpage/icons/icons.jsx
@@ -0,0 +1,17 @@
+import "./icvons.scss"
+import {TbBrandFacebook} from "react-icons/tb"
+
+
+const Icons =() =>{
+
+    return(
+        <div className="socialicons">
+            <div className="right_icons">
+                <TbBrandFacebook></TbBrandFacebook>
+            </div>
+            <div className="left_icons">
+
+            </div>
+        </div>
+    )
+}
