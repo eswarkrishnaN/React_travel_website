09ca5545eae91f6164639e6c45199596be615523
unknow 9090 <nsnshanmukh52@gmail.com>
2023-05-01 13:51:54 +0530

    backend page

---
+++ update_data_page/update/python/app.py
@@ -0,0 +1,17 @@
+from flask import Flask,request,jsonify
+from pymongo  import MongoClient
+
+#flask --app example --debug run
+app =Flask(__name__)
+
+
+
+
+
+
+
+
+    
+
+if __name__ =="__main__":
+    app.run(debug=True)
--- update_data_page/update/src/App.js
+++ update_data_page/update/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
+  const[imageur,setimageurl] = useState('');
+  const[title,setitle] = useState('');
+  const[loaction,setlocation] = useState('');
+  const[grade,setgrade] = useState('');
+  const[fees,setFees] = useState('');
+  const[description,setDescription] = useState('');
+
+
+
+  const from_submition = async (event) =>{
+    event.preventDefault();
+    const data ={imageur,title,loaction,grade,fees,description};
+  
+    const response = await fetch('/add-data', {
+    
+      method: 'Post',
+      headers :{
+        'Content-type' :'application/json'
+      },
+      body:JSON.stringify(data)
+    });
+    const result = await response.json();
+    console.log(result);
+    if(response.ok){
+      console.log('sucess');
+    }else{
+      console.log('erroe');
+    }
+  
+  }
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-form">
+      <form OnSubmitTest={from_submition} >
+        <label>
+          <span>Image_url:</span>
+          <input  value ={imageur} onChange={(event) => setimageurl( event.target.value)} type="url" placeholder="Url"   required />
+        </label>
+        <label>
+        <span> Titel:</span>
+          <input value={title} onChange={(event) => setitle(event.target.value)} type="text" placeholder="Title"    />
+        </label>
+        <label>
+          <span>location:</span>
+          <input value={loaction} onChange={(event) => setlocation(event.target.value)}type="text" placeholder=" loaction"    required />
+        </label>
+        <label>
+         <span>grade:</span> 
+          <input  value={grade} onChange={(event) => setgrade(event.target.value)}type="text" placeholder=" Chill"  required />
+        </label>
+        <label>
+          <span>Fees:</span>
+          <input value={fees} onChange={(event) => setFees(event.target.value)} type="text"  placeholder="price" required />
+        </label>
+        <label>
+          <span>Description:</span>
+          <input value={description} onChange={(event) =>setDescription(event.target.value)} type="text" className="descrition"  placeholder=" enter some information" required  />
+        </label>
+        <button type="submit">Log In</button>
+      </form>
     </div>
   );
 }
